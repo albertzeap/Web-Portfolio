59b3f59442098d5a1904f4b7aea9876445c4806a
Albert <albertzeap@gmail.com>
2022-08-23 23:41:33 -0700

    Created header structure

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header__left">
+            <img
+            src=""
+            alt=""
+            />
+        </div>
+        
+        <div className="header__center">
+
+
+
+        </div>
+
+        <div className="header__right">
+
+
+            
+        </div>
+
+
+
+    </div>
+
+  )
+}
+
+export default Header
