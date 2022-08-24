0734fae92099024327deda13fe6dc482595f4857
Albert <albertzeap@gmail.com>
2022-08-24 02:59:10 -0700

    Finished basic header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     //BEM Naming Conventions
     <div className="app">
+      
       <Header />
 
       <div className="app__body">
-
-
+        
       </div>
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,32 @@
 import React from 'react'
+import "./Header.css"
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 
 function Header() {
   return (
     <div className="header">
         <div className="header__left">
-            <img
-            src=""
-            alt=""
-            />
+            <a href="/">
+                <img src="https://live.staticflickr.com/65535/52306659962_edec6176a3_o.png" alt=""/>
+            </a>
         </div>
         
         <div className="header__center">
-
-
-
+            <h2>Web Developer</h2>
         </div>
 
         <div className="header__right">
+            <IconButton href="https://github.com/albertzeap" target="_blank" rel="noopener">
+                <GitHubIcon/>
+            </IconButton>
+            <IconButton href="https://www.linkedin.com/in/albert-paez-30501a1b5" target="_blank" rel="noopener">
+                <LinkedInIcon/>
+            </IconButton>
 
 
-            
         </div>
 
 
