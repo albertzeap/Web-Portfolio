06789a3f6f3ef00bf00450d68d1b809d883fec85
Albert <albertzeap@gmail.com>
2022-08-27 15:36:49 -0700

    fixed section placements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-// import { Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import Header from "./Components/Header"
 import Sidebar from "./Components/Sidebar";
 import Experience from './Pages/Experience';
 import Projects from './Pages/Projects';
 import Welcome from './Pages/Welcome';
-// import videobackground from "./images/videobackground.mp4"
+
+
+
 
 function App() {
   return (
-   
+ 
     <div id="app">
       <Header />
       <div className="app__body">
@@ -21,6 +23,7 @@ function App() {
       </div>
 
     </div> 
+
   );
 }
 
